438211843e3f6ceb1bb5157da2ae8f99a96d10d7
yehualashet <yehualashetab@gmail.com>
2024-07-01 00:07:34 +0200

    chore: Remove unused code and update home component template

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'http://localhost:3000',
   application:
   {
-    name: 'angular-starter',
+    name: 'customer Onboarding Application',
     angular: 'Angular 18.0.5',
     bootstrap: 'Bootstrap 5.3.3',
     fontawesome: 'Font Awesome 6.5.2',
